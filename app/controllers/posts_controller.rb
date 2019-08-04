b09557a2df8f17611bfba6213332627ca72850c8
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-04 17:12:39 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
 	def new
 		@post = Post.new
 	end
-  
+
   def edit
     @post = Post.find(params[:id])
   end
@@ -19,15 +19,15 @@ class PostsController < ApplicationController
     @post = Post.create(post_params)
     redirect_to post_path(@post)
   end
-   
+
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to post_path(@post)
   end
-   
+
   private
-   
+
   def post_params
     params.require(:post).permit(:title, :description)
   end
