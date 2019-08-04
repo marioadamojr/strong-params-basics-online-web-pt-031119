7d6518b9fc8749945f64bba2272be887f00e4c5e
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-04 17:14:00 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,13 +16,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
+    @post = Post.create(post_params(:title, :description))
     redirect_to post_path(@post)
   end
 
   def update
     @post = Post.find(params[:id])
-    @post.update(post_params)
+    @post.update(post_params(:title))
     redirect_to post_path(@post)
   end
 
